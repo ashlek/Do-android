3d1b46e53dc252b8dc0bde5145d7c179bffcadb3
ETD-P\do <azn.alex@hotmail.fr>
2016-11-07 17:21:22 +0100

    Slide 23

--- app/src/main/java/com/example/etd_pdo/ado/do_mobile.java
+++ app/src/main/java/com/example/etd_pdo/ado/do_mobile.java
@@ -1,50 +1,114 @@
 package com.example.etd_pdo.ado;
 
 import android.app.DatePickerDialog;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.text.format.DateUtils;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import java.text.DateFormat;
 import java.util.Date;
 
 public class do_mobile extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_do_mobile);
 
-        TextView tv_hw = (TextView)findViewById(R.id.tv_hello_world);
-        String now = DateUtils.formatDateTime(getApplicationContext(),(new Date()).getTime(), DateFormat.FULL);
-        tv_hw.setText(getString(R.string.hello_world)+now);
+        TextView tv_hw = (TextView) findViewById(R.id.tv_hello_world);
+        String now = DateUtils.formatDateTime(getApplicationContext(), (new Date()).getTime(), DateFormat.FULL);
+        tv_hw.setText(getString(R.string.hello_world) + now);
 
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
 
+    public void QuandTuCliques(View v) {
+        final TextView tv_hw = (TextView) findViewById(R.id.tv_hello_world);
+
+        DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
+                // arg1 = year
+                // arg2 = month
+                // arg3 = day
+                tv_hw.setText(getString(R.string.hello_world) + arg1 + "/" + arg2 + "/" + arg3);
+            }
+        };
+        Toast.makeText(getApplicationContext(), getString(R.string.msg), Toast.LENGTH_LONG).show();
+        DatePickerDialog dpd = new DatePickerDialog(this, myDateListener, 2016, 11, 07);
+        dpd.show();
+        notification_test();
 
     }
 
-     public void QuandTuCliques(View v){
-        final TextView tv_hw = (TextView)findViewById(R.id.tv_hello_world);
+    public void notification_test() {
+        NotificationCompat.Builder notif = new NotificationCompat.Builder(this);
+        notif.setContentTitle("Notif");
+        notif.setContentText("Notif");
+        notif.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_euro_symbol_black_18dp));
+        NotificationManager notifmanager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notifmanager.notify(1, notif.build());
+    }
+/*
+    @Override
+    public void onStart() {
+        super.onStart();
 
-     DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
-             @Override
-             public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
-                 // arg1 = year
-                 // arg2 = month
-                 // arg3 = day
-                 tv_hw.setText(getString(R.string.hello_world)+arg1+" "+arg2+" "+arg3);
-             }
-         };
-           Toast.makeText(getApplicationContext(),getString(R.string.msg),Toast.LENGTH_LONG).show();
-         DatePickerDialog dpd = new DatePickerDialog(this,myDateListener,2016,11,07);
-         dpd.show();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "do_mobile Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.etd_pdo.ado/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
 
-     }
+    @Override
+    public void onStop() {
+        super.onStop();
 
-
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "do_mobile Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.etd_pdo.ado/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }*/
 }
