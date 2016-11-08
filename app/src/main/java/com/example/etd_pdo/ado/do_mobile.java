5b7ea5db8ed507b308e873cdb42268b9b1bc0db4
ETD-P\do <azn.alex@hotmail.fr>
2016-11-08 11:33:03 +0100

    Intent

--- app/src/main/java/com/example/etd_pdo/ado/do_mobile.java
+++ app/src/main/java/com/example/etd_pdo/ado/do_mobile.java
@@ -3,12 +3,15 @@ package com.example.etd_pdo.ado;
 import android.app.DatePickerDialog;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.NotificationCompat;
 import android.text.format.DateUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TextView;
@@ -36,7 +39,7 @@ public class do_mobile extends AppCompatActivity {
 
         TextView tv_hw = (TextView) findViewById(R.id.tv_hello_world);
         String now = DateUtils.formatDateTime(getApplicationContext(), (new Date()).getTime(), DateFormat.FULL);
-        tv_hw.setText(getString(R.string.hello_world) + now);
+        tv_hw.setText(getString(R.string.hello_world) + now );
 
 
         // ATTENTION: This was auto-generated to implement the App Indexing API.
@@ -44,7 +47,7 @@ public class do_mobile extends AppCompatActivity {
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    public void QuandTuCliques(View v) {
+    public void changerH(){
         final TextView tv_hw = (TextView) findViewById(R.id.tv_hello_world);
 
         DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
@@ -53,63 +56,53 @@ public class do_mobile extends AppCompatActivity {
                 // arg1 = year
                 // arg2 = month
                 // arg3 = day
+                arg2 = arg2%12 + 1;
                 tv_hw.setText(getString(R.string.hello_world) + arg1 + "/" + arg2 + "/" + arg3);
             }
         };
         Toast.makeText(getApplicationContext(), getString(R.string.msg), Toast.LENGTH_LONG).show();
         DatePickerDialog dpd = new DatePickerDialog(this, myDateListener, 2016, 11, 07);
         dpd.show();
-        notification_test();
+    }
 
+    public void QuandTuCliques(View v) {
+        //changerH();
+        notification_test();
+        intentSecondeActivity();
+
+    }
+
+    public void Locate(View v){
+     startActivity(new Intent(Intent.ACTION_VIEW,Uri.parse("geo:0,0?q=Londre")));
     }
 
     public void notification_test() {
         NotificationCompat.Builder notif = new NotificationCompat.Builder(this);
-        notif.setContentTitle("Notif");
-        notif.setContentText("Notif");
+        notif.setContentTitle("Here comes the Money");
+        notif.setContentText("Give me the dollar bill yo..");
         notif.setSmallIcon(R.drawable.ic_euro_symbol_white_18dp);//BitmapFactory.decodeResource(getResources(), R.drawable.ic_euro_symbol_black_18dp));
 
         NotificationManager notifmanager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         notifmanager.notify(1, notif.build());
     }
-/*
-    @Override
-    public void onStart() {
-        super.onStart();
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "do_mobile Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.example.etd_pdo.ado/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
-    @Override
-    public void onStop() {
-        super.onStop();
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.toast) {
+            Toast.makeText(getApplicationContext(), "Recherche depuis l'action bar !", Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "do_mobile Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.example.etd_pdo.ado/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
-    }*/
+    public void intentSecondeActivity(){
+        Intent i = new Intent(this, SecondeActivity.class);
+        startActivity(i);
+    }
 }
